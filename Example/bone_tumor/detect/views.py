9d6986f7b488c72172c2ec82390e6c646db3412a
dghant <trandonggiahan2003@gmail.com>
2025-03-30 04:20:24 +0700

    chạy django demo lần 1

--- Example/bone_tumor/detect/views.py
+++ Example/bone_tumor/detect/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from .models import UploadedImage
 
 def upload_image(request):
-    """Xử lý tải ảnh lên."""
+    """Xử lý tải ảnh lên mà không lưu vào database."""
     if request.method == "POST" and request.FILES.get("image"):
-        image_file = request.FILES["image"]
-        uploaded_image = UploadedImage.objects.create(image=image_file)
+        image_file = request.FILES["image"]  # Lấy file ảnh
 
-        return render(request, "result.html", {"image": uploaded_image})
+        return render(request, "result.html", {"image_url": image_file})
 
     return render(request, "upload.html")
